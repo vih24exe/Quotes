e919495bedc08067d3dd9c47820f045e8103f21f
vih24exe <vih24exe@hotmail.com>
2019-06-13 21:55:00 -0600

    quotes

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.lenght" maxQuotes=10>
+        </app-header>
+            
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+    export default {
+
+        components: {
+            appHeader: Header
+        },
+
+        data: function(){
+            return{
+                quotes:[ 
+                    'Hola mundo cruel',
+                ],
+                maxQuotes: 10
+            }
+        }
+        
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row">
+        <div class="col-sm12">
+            <h3>Citas agregadas</h3>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60"
+                aria-valuemin="0" aria-valuemax="100"
+                :style="{width: (quoteCount/ maxQuote) * 100 + '%'}">
+                {{quoteCount / maxQuote}} </div>
+                </div>
+            </div>
+        </div>
+</template>
+<script>
+export default {
+    props:['quotesCount', 'maxQuote'],
+}
+</script>
+
